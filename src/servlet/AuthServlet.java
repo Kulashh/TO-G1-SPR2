3b1eb1a6dfc5e8e0fe42023729a3adf539e5b70b
Kulashh <k.bazarbayeva@list.ru>
2024-02-12 04:29:45 +0600

    Sprint2

---
+++ src/servlet/AuthServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import db.DbManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/auth")
+public class AuthServlet extends HomeServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("user_email");
+        String password = req.getParameter("user_password");
+        String message = DbManager.auth(email, password);
+        req.setAttribute("message", message);
+        req.getRequestDispatcher(".jsp").forward(req,resp);
+    }
+}
